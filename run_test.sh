e50e9c52f7e2b0b6204ff0d37d5f77bd1db0bd07
VictorTao1998 <jit124@ucsd.edu>
2022-02-26 18:34:01 -0800

    test

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,7 @@
 export PYTHONWARNINGS="ignore"
 
 python /jianyu-fast-vol/activezero_vanilla/test_psmnet_with_confidence.py   \
---config-file 'configs/remote_test.yaml' \
+--config-file '/jianyu-fast-vol/activezero_vanilla/configs/remote_test.yaml' \
 --model '/jianyu-fast-vol/eval/sim_cv_bce_2/train_sim_cv_bce/models/model_100000.pth' \
 --output '/jianyu-fast-vol/eval/sim_cv_bce_2/test_sim_cv_bce' \
 --exclude-bg \
