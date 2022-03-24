9282340f51bd0c5614069a7626d29cdffb2f95e0
VictorTao1998 <jit124@ucsd.edu>
2022-03-23 23:01:41 -0700

    p

---
+++ run_p2.sh
@@ -0,0 +1,7 @@
+export PYTHONWARNINGS="ignore"
+
+python /jianyu-fast-vol/activezero_vanilla/reproj_without_sim_gt/train_psmnet_ir_reproj_p2.py   \
+--config-file '/jianyu-fast-vol/activezero_vanilla/configs/remote_train_primitive_randscenes.yaml' \
+--logdir '/jianyu-fast-vol/eval/final_exp/p2_wo_simgt' \
+--gaussian-blur \
+--color-jitter \
