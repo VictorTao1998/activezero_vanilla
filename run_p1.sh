55f38d2600def257dace6f296091b4b1441dd1b5
VictorTao1998 <jit124@ucsd.edu>
2022-03-23 23:21:33 -0700

    p

--- run_p1.sh
+++ run_p1.sh
@@ -1,7 +1,7 @@
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch --nproc_per_node=2 /jianyu-fast-vol/activezero_vanilla/train_psmnet_ir_reproj_p1.py   \
+python -m torch.distributed.launch --nproc_per_node=2 /jianyu-fast-vol/activezero_vanilla/train_psmnet_ir_reproj_p1.py \
 --config-file '/jianyu-fast-vol/activezero_vanilla/configs/remote_train_primitive_randscenes.yaml' \
 --logdir '/jianyu-fast-vol/eval/final_exp/p1_wo_simgt' \
 --gaussian-blur \
---color-jitter \
+--color-jitter
--- run_p2.sh
+++ run_p2.sh
@@ -1,7 +1,7 @@
 export PYTHONWARNINGS="ignore"
 
-python -m torch.distributed.launch --nproc_per_node=2 /jianyu-fast-vol/activezero_vanilla/train_psmnet_ir_reproj_p2.py   \
+python -m torch.distributed.launch --nproc_per_node=2 /jianyu-fast-vol/activezero_vanilla/train_psmnet_ir_reproj_p2.py \
 --config-file '/jianyu-fast-vol/activezero_vanilla/configs/remote_train_primitive_randscenes.yaml' \
 --logdir '/jianyu-fast-vol/eval/final_exp/p2_wo_simgt' \
 --gaussian-blur \
---color-jitter \
+--color-jitter
